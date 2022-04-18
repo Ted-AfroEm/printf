22e8f58fcd90d2d6a5513fc6720e82237284f304
KalabSisaySE <kyimusic11@gmail.com>
2022-04-19 02:55:54 +0300

    test

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    	char a = 'a';
+    	char b = 'b';
+    	char c = 'c';
+    
+    	int integer = 345;
+    	char s[] = "hello";
+    	char d[] = "another word";
+    
+   	 _printf("first: %c, second: %c, third: %s, fourth: %s\n", a, b, s, d);
+  
+	return(0);
+}
