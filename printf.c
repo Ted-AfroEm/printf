a7dc6b824d4f108bf08fef39fa81776393c35834
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-19 00:43:14 +0300

    print char using %c

--- printf.c
+++ printf.c
@@ -9,24 +9,52 @@
 int _printf(const char *format, ...)
 {
 	int i;
-	int n;
+	int arg_count;
 	const char *ptr;
+	va_list p;
 
 	i = 0;
-	n = 0;
+	arg_count = 0;
 	ptr = format;
 	while (*ptr != '\0')
 	{
 
 		if ((*ptr == '%') && (*(ptr + 1) != '%'))
-			n++;
+			arg_count++;
 
-		_putchar(*ptr);
+		/*_putchar(*ptr);*/
 		ptr++;
 		i++;
 	}
 
-	printf("Number of %%: %i\n", n);
+	va_start(p, format);
+	i = 0;
+	ptr = format;
+	
+	while(*ptr != '\0')
+	{
+
+		if ((*ptr == '%') && (*(ptr + 1) != '%'))
+		{	
+			/*printf("%c", *(ptr+1));*/
+			if(*(ptr + 1) == 'c')
+			{
+				/*printf("here2\n");*/
+				while(i < arg_count)
+				{
+					char c = va_arg(p, int);
+					_putchar(c);
+					/*printf("here3\n");*/
+					ptr = ptr + 2;
+					i++;
+				}
+			}
+		}
+		_putchar(*ptr);
+		ptr++;
+	}
+
+	printf("Number of %%: %i\n", arg_count);
 
 	return (i);
 }
---
+++ test.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int main(void)
+{
+	char sign = '$';
+	
+	_printf("First test\n");
+	_printf("This is %c \n");
+	_printf("print char %c \n", sign);
+	return(0);
+}
