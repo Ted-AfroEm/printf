42cc850b8772aab3e047d75122f28ac52bd5ceb1
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-19 07:41:43 +0300

    refactor code

--- printf.c
+++ printf.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 int countArguments(const char *ptr);
+int checkSpecifier(const char *ptr, va_list p);
+void printSingleChar(va_list p);
 
 /**
  * _printf - clone of printf function
@@ -12,6 +14,7 @@ int _printf(const char *format, ...)
 {
 	int i;
 	int arg_count;
+	int flagSpecifier;
 	const char *ptr;
 	va_list p;
 
@@ -24,26 +27,28 @@ int _printf(const char *format, ...)
 	va_start(p, format);
 	i = 0;
 	ptr = format;
+	flagSpecifier = 0;
 
-	while(*ptr != '\0')
+	while (*ptr != '\0')
 	{
 		if ((*ptr == '%') && (*(ptr + 1) != '%'))
 		{
-			if(*(ptr + 1) == 'c')
-				{
-					char c = va_arg(p, int);
-					_putchar(c);
-					ptr++;
-				}
+			flagSpecifier = checkSpecifier(ptr, p);
+			if (flagSpecifier == 1)
+				ptr++;
+		}
+		/* if two % come condecutively print one %*/
+		else if ((*ptr == '%') && (*(ptr + 1) == '%'))
+		{
+			_putchar(*ptr);
+			ptr++;
 		}
 		else
 			_putchar(*ptr);
 		ptr++;
 		i++;
 	}
-
-	printf("Number of %%: %i\n", arg_count);
-
+	printf("Arguments: %i\n", arg_count);
 	return (i);
 }
 
@@ -67,4 +72,35 @@ int countArguments(const char *ptr)
 	}
 
 	return (arg_count);
+}
+
+/**
+ * checkSpecifier - find one of the format specifier
+ * @ptr: ptr to string
+ * @p: va_list
+ *
+ * Return: flag 1 for true else 0 for false
+ */
+int checkSpecifier(const char *ptr, va_list p)
+{
+	/*_putchar(*ptr);*/
+	if (*(ptr + 1) == 'c')
+	{
+		printSingleChar(p);
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * printSingleChar - print a single character
+ * @p: va_list
+ *
+ * Return: nothing
+ */
+void printSingleChar(va_list p)
+{
+	char c = va_arg(p, int);
+
+	_putchar(c);
 }
