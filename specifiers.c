d0ce7d871af9d9477f1ee457c0da35c254a66e5b
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-19 21:13:10 +0300

    fix betty erroes

--- specifiers.c
+++ specifiers.c
@@ -20,7 +20,7 @@ int checkSpecifier(const char *ptr, va_list args)
 		printStringChar(args);
 		return (1);
 	}
-	else if(*(ptr + 1) == 'd')
+	else if (*(ptr + 1) == 'd')
 	{
 		printDecimal(args);
 		return (1);
