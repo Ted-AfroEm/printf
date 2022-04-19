74d27669a03920305562bc617e5f723fca53aa15
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-19 07:45:44 +0300

    push test file

--- test.c
+++ test.c
@@ -7,8 +7,14 @@ int main(void)
 	char sign2 = '#';
 		
 	_printf("First test\n");
-	_printf("This is %c \n");
+	_printf("This is %c\n");
 	_printf("print char %c \n", sign1);
-	_printf("print char %c and %c", sign1, sign2); 
+	_printf("print char %c- and %c-\n", sign1, sign2);
+	_printf("print char %%%c and %c\n", sign1, sign2);
+	printf("First test\n");
+	/*printf("This is %c\n");*/
+	printf("print char %c \n", sign1);
+	printf("print char %c- and %c-\n", sign1, sign2);
+	printf("print char %%%c and %c\n", sign1, sign2);
 	return(0);
 }
