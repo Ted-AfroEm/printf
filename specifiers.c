2c6024bcc9d14b86a302c60bfd63479066f9bdb6
KalabSisaySE <kyimusic11@gmail.com>
2022-04-19 20:27:31 +0300

    %d

--- specifiers.c
+++ specifiers.c
@@ -52,3 +52,38 @@ void printStringChar(va_list args)
 		c++;
 	}
 }
+
+/**
+ * printDecimal - print a decimal number
+ * @args: va_list
+ *
+ * Return: nothing
+ */
+void printDecimal(va_list args)
+{
+
+	int  n = va_arg(args, int);
+
+	print(n);
+}
+
+/**
+ * printNumber - prints number
+ * @n: the number to be printed
+ *
+ * Return: nothing
+ */
+
+void printNumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print(n / 10);
+
+	_putchar(n % 10 + '0');
+}
