9dcbd23699504b5451b6fc930bf8c9ffbe7a1622
KalabSisaySE <kyimusic11@gmail.com>
2022-04-19 20:04:22 +0300

    updated header file

--- main.h
+++ main.h
@@ -1,7 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int checkSpecifier(const char *ptr, va_list args);
+void printSingleChar(va_list args);
+void printStringChar(va_list args);
+void printDecimal(va_list args);
+void printNumber(va_list args);
 
 #endif
+
