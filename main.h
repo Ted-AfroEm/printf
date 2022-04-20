b5341d1b4c4519cfad515ba037fd8bca0ab8b271
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-20 04:51:46 +0300

    change printStringChar from void to int return type

--- main.h
+++ main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int checkSpecifier(const char *ptr, va_list args);
 void printSingleChar(va_list args);
-void printStringChar(va_list args);
+int printStringChar(va_list args);
 void printDecimal(va_list args);
 void printNumber(int n);
 
